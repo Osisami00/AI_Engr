80d4c884351ff609adaebc0c8fc67868d8bb661d
Osisami <osisamimichael1@gmail.com>
2025-08-15 14:53:41 +0100

    Weekend Assignment

---
+++ My_Task/week_2/task7_answers.py
@@ -0,0 +1,16 @@
+# Task1: Student Bio Data Storage
+# collect student bio
+student_bio = {}
+bio_without_courses ={}
+list_of_courses = []
+name = input("Hi! Kindly enter your name: ")
+age = int(input("Enter your age: "))
+sex= input(("What is your gender? "))
+course_title = input("Enter course of study:")
+bio_without_courses.append(name)
+bio_without_courses.append(name)
+bio_without_courses.append(name)
+print(bio_without_courses)
+print(f"{name}\n{age}\n{sex}\n{course_title}")
+
+
