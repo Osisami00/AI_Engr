e81627b88e272ec00d6fd298fccba5bc845be56f
Osisami <osisamimichael1@gmail.com>
2025-09-01 09:39:03 +0100

    My Calculator Project

---
+++ My_Task/week_4/main.py
@@ -0,0 +1,70 @@
+# This is the module with CSV file path and user prompt
+from pathlib import Path
+import csv
+import file_ops
+import os
+
+def local_path():
+    folder = Path('participant_pkg')
+    return folder / 'participant.csv'
+
+
+
+def participant_info():
+    participant = {}
+    try:    
+        while True:
+            name = input("Enter your name: ").strip().title()
+            if name.isalpha() == False:
+                print("Error! Enter a correct name.")
+            else:    
+                participant["name"] = name
+                break
+
+        while True:
+            age = input(f"Dear {participant["name"]}, kindly enter your age: ")
+        
+            if int(age) <= 1 or int(age) > 120:
+                print("That can't be your age. Enter your age")
+            # if age.isdigit():
+            #     print("Error! Age should be a number")
+                participant["age"] = age
+            else:
+                break
+        
+        while True:
+            phone_number = input("Enter your phone number: ")
+            if len(phone_number) != 11:
+                print("Phone number should be 11digits")
+            elif phone_number.isalpha == True:
+                print("Phone Number cannot be a letter")
+                participant["phone_number"] = phone_number
+            else:
+                break
+            
+        while True:
+            track =input("Enter your track: ").strip().upper()
+            if track.isalpha == False:
+                print("Letter should be in words. Try again.")
+                participant["track"] = track
+            else: 
+                break
+    except ValueError as e:
+        print(e)
+
+participant_info()
+
+
+
+
+
+def partcipant_list(n):
+    path = local_path()
+    for i in range(n):
+        participant_dic = participant_info()
+        file_ops.save_participant(path, participant_dic)
+partcipant_list(2)
+
+
+
+
---
+++ My_Task/week_4/participant_pkg/file_ops.py
@@ -0,0 +1,14 @@
+# This module is used to define funcs
+
+
+
+while True:
+    
+    
+
+
+
+# def save_participant(path, participant_dict):
+#     with open(participant, "w", newline="", encoding="utf-8") as f:
+        
+ 
