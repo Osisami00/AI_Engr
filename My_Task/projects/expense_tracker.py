0746e87c8d95d215ebb6afadf89c252549a3c9a2
Osisami <osisamimichael1@gmail.com>
2025-09-02 09:24:52 +0100

    Expense Tracker Cont..

--- My_Task/projects/expense_tracker.py
+++ My_Task/projects/expense_tracker.py
@@ -38,111 +38,113 @@ balance = 10,000
 
 def expense_tracker():
     expenses={}
-    try:      
-        while True:
-            item1 = input("Enter first item: ").strip()
-            if item1 =="":
-                print("Item cannot empty")
-                continue
-            elif item1.isdigit():
-                print("Item cannot be a number")
-                continue
-            else:
-                pass
-            price1 = int(input("Enter price of item: "))
-            if price1 =="":
-                print("Item cannot empty")
-                continue
-            elif price1.isalpha():
-                print("Price cannot be a word")
-                continue
-            else:
-                expenses["item1"] = int(price1)
+    try: 
+        while True:     
+            # FOR FIRST ITEM AND PRICE
+            while True:
+                item1 = input("Enter first item: ").strip()
+                if item1 =="":
+                    print("Item cannot empty")
+                    
+                elif item1.isdigit():
+                    print("Item cannot be a number")
+                else:
+                    break
 
+            while True:
+                price1 = input("Enter price of item: ")
+                if price1 =="":
+                    print("Price cannot empty")
+                    
+                elif price1.isalpha():
+                    print("Price cannot be a word")
+                    
+                else:
+                    
+                    expenses["item1"] = int(price1)
+                    break
+        
+                # FOR SECOND ITEM AND PRICE
+            while True:
+                    # FOR ITEM2
+                item2 = input("Enter second item: ").strip()
+                if item2 =="":
+                    print("Item cannot empty")
+                elif item2.isdigit():
+                    print("Item cannot be a number")
+                else:
+                    break
 
+                    # FOR PRICE2
+            while True:
+                price2 = input("Enter price of item: ")
+                if price2 =="":
+                    print("Item cannot empty")
+                elif price2.isalpha():
+                    print("Price cannot be a word")
+                else:
+                    expenses["item2"] = int(price2)
+                    break
 
-            item2 = input("Enter second item: ").strip()
-            if item2 =="":
-                print("Item cannot empty")
-                continue
-            elif item2.isdigit():
-                print("Item cannot be a number")
-                continue
-            else:
-                pass 
-            price2 = int(input("Enter price of item: "))
-            if price2 =="":
-                print("Item cannot empty")
-                continue
-            elif price2.isalpha():
-                print("Price cannot be a word")
-                continue
-            else:
-                expenses["item2"] = int(price2)
+                # FOR third ITEM AND PRICE
+            while True:
+                item3 = input("Enter third item: ").strip()
+                if item3 =="":
+                    print("Item cannot empty")
+                elif item3.isdigit():
+                    print("Item cannot be a number")
+                else:
+                    break
+            while True:
+                price3 = input("Enter price of item: ")
+                if price3 =="":
+                    print("Item cannot empty")
+                    continue
+                elif price3.isalpha():
+                    print("Price cannot be a word")
+                    continue
+                else:
+                    expenses["item3"] = int(price3)
+                    break
+                    
+                    # FOR Fourth ITEM AND PRICE
+            while True:
+                item4 = input("Enter fourth item: ").strip()
+                if item4 =="":
+                    print("Item cannot empty")
+                elif item4.isdigit():
+                    print("Item cannot be a number")
+                else:
+                    break
+            while True:
+                price4 = input("Enter price of item")
+                if price4 =="":
+                    print("Item cannot empty")
+                elif price4.isalpha():
+                    print("Price cannot be a word")
+                else:
+                    expenses["item4"] = int(price4)
+                    break
 
-
-            item3 = input("Enter third item: ").strip()
-            if item3 =="":
-                print("Item cannot empty")
-                continue
-            elif item3.isdigit():
-                print("Item cannot be a number")
-                continue
-            else:
-                item3 = int(item3)
-            price3 = int(input("Enter price of item: "))
-            if price3 =="":
-                print("Item cannot empty")
-                continue
-            elif price3.isalpha():
-                print("Price cannot be a word")
-                continue
-            else:
-                expenses["item3"] = int(price3)
-
-
-
-            item4 = input("Enter fourth item: ").strip()
-            if item4 =="":
-                print("Item cannot empty")
-                continue
-            elif item4.isdigit():
-                print("Item cannot be a number")
-                continue
-            else:
-                item4 = int(item4)
-            price4 = int(input("Enter price of item"))
-            if price4 =="":
-                print("Item cannot empty")
-                continue
-            elif price4.isalpha():
-                print("Price cannot be a word")
-                continue
-            else:
-                expenses["item4"] = int(price4)
-
-
-
-            item5 = input("Enter fifth item: ").strip()
-            if item5 =="":
-                print("Item cannot empty")
-                continue
-            elif item5.isdigit():
-                print("Item cannot be a number")
-                continue
-            else:
-                pass
-            price5 = int(input("Enter price of item"))
-            if price5 =="":
-                print("Item cannot empty")
-                continue
-            elif price5.isalpha():
-                print("Price cannot be a word")
-                continue
-            else:
-                expenses["item5"] = int(price5)
-
-
+                    # FOR Fourth ITEM AND PRICE
+            while True:
+                item5 = input("Enter fifth item: ").strip()
+                if item5 =="":
+                    print("Item cannot empty")
+                elif item5.isdigit():
+                    print("Item cannot be a number")
+                else:
+                    break
+            while True:
+                price5 = input("Enter price of item")
+                if price5 =="":
+                    print("Item cannot empty")
+                elif price5.isalpha():
+                    print("Price cannot be a word")
+                else:
+                    expenses["item5"] = int(price5)
+                    break
+            
             # expenses["item5"] = int(price5)
             total_cost = expenses["item1"] + expenses["item2"]+ expenses["item3"] + expenses["item4"] +expenses["item5"]
             expenses = balance - total_cost
